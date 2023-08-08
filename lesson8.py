b31ed8a26c808cc0519f5d163abe20e51d8fa4b9
Codelobster <codelobster@yandex.ru>
2023-08-08 22:49:20 +0300

    Decided

--- lesson8.py
+++ lesson8.py
@@ -1,1 +1,33 @@
+# Задача 38: Дополнить телефонный справочник возможностью изменения и удаления данных. 
+# Пользователь также может ввести имя или фамилию, и Вы должны реализовать функционал для изменения и удаления данных
 
+print('out() - вывод всего справочника')
+print('out_name("Name") - поиск по имени')
+print('delete("Name") - удалить номер')
+print('add("Name", Number) - добавить/изменить номер')
+
+dict_number = {'Иванов':111, 'Петров':333, 'Сидоров':666}
+
+def out(TD = dict_number):
+    for i in TD:
+        print(i, ' - ', TD[i])
+    eval(input())
+
+def out_name(name, dn = dict_number):
+    print(dn[name])
+    eval(input())
+    
+def delete(name, dn = dict_number):
+    del dn[name]
+    print('Удалено')
+    eval(input())
+
+def add(name, number):
+    print('Добавлено или изменено')
+    dict_number[name] = number
+    eval(input())
+
+def stop():
+    print('Выход')
+
+eval(input())
